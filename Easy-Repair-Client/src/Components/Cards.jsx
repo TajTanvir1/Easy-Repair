78d710668679eb95f986f615e53e7d6e85b90ff9
tajtanvvir1 <tanvirah1223@gmail.com>
2024-09-04 22:56:06 +0600

    Card Added'

---
+++ Easy-Repair-Client/src/Components/Cards.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cards = () => {
+   return (
+      <div>
+         <p><span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis animi ea obcaecati voluptatem, eveniet voluptas. Et at neque perferendis non.</span><span>Aspernatur est eum distinctio? Recusandae, molestiae dignissimos eveniet distinctio quae, rerum quaerat tempore obcaecati nihil quia vel optio at assumenda!</span><span>Commodi voluptatibus corporis incidunt. Ab laboriosam eum ullam illum blanditiis mollitia iusto vel consequatur tempora explicabo eaque, odio fuga nulla?</span><span>Ut eaque quae maiores ullam reiciendis. Necessitatibus, recusandae laudantium? Dicta ut nihil illum quas quisquam provident praesentium nisi. Nihil, commodi?</span><span>Voluptates odio blanditiis eaque asperiores assumenda animi magnam perferendis recusandae facere quas, harum, quod cumque dignissimos velit enim praesentium. Qui.</span></p>
+      </div>
+   );
+};
+
+export default Cards;
