ff6ff8f8d801d52f99ed12d5909c6a2c3f8143be
tajtanvvir1 <tanvirah1223@gmail.com>
2024-09-13 22:46:58 +0600

    Register update

--- Easy-Repair-Client/src/Pages/Login/Register.jsx
+++ Easy-Repair-Client/src/Pages/Login/Register.jsx
@@ -5,15 +5,14 @@ const Register = () => {
          <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
                <img
-                  src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                  src=""
                   className="max-w-sm rounded-lg shadow-2xl" />
                <div>
-                  <h1 className="text-5xl font-bold">Box Office News!</h1>
+                  <h1 className="text-5xl font-bold">Register Here</h1>
                   <p className="py-6">
-                     Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                     quasi. In deleniti eaque aut repudiandae et a id nisi.
+                     
                   </p>
-                  <button className="btn btn-primary">Get Started</button>
+                  <button className="btn btn-primary">Register</button>
                </div>
             </div>
          </div>
