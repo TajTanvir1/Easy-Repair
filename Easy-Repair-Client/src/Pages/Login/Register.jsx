a93fe469bdd5b3c4ce3417fe4facf84ac39673d9
tajtanvvir1 <tanvirah1223@gmail.com>
2024-09-14 23:57:37 +0600

    Login Func added

--- Easy-Repair-Client/src/Pages/Login/Register.jsx
+++ Easy-Repair-Client/src/Pages/Login/Register.jsx
@@ -1,10 +1,26 @@
 import { Link } from 'react-router-dom';
 import img from '../../assets/images/login/login.svg'
+import { useContext } from 'react';
+import { AuthContext } from '../../Providers/AuthProvider';
 
 const Register = () => {
 
+   const {createUser} = useContext(AuthContext);
+
 const handleRegister = e =>{
    e.preventDefault();
+   const form = e.target;
+   const name = form.name.value;
+   const email = form.email.value;
+   const password = form.password.value;
+   console.log(name, email, password);
+
+   createUser(email, password)
+   .then(result => {
+      const user = result.user;
+      console.log(user)
+   })
+   .catch(error => console.log(error))
 }
 
    return (
@@ -21,19 +37,19 @@ const handleRegister = e =>{
                         <label className="label">
                            <span className="label-text">Name</span>
                         </label>
-                        <input type="text" placeholder="Name" className="input input-bordered" required />
+                        <input type="text" name='name' placeholder="Name" className="input input-bordered" required />
                      </div>
                      <div className="form-control">
                         <label className="label">
                            <span className="label-text">Email</span>
                         </label>
-                        <input type="email" placeholder="email" className="input input-bordered" required />
+                        <input type="email" name='email' placeholder="email" className="input input-bordered" required />
                      </div>
                      <div className="form-control">
                         <label className="label">
                            <span className="label-text">Enter Password</span>
                         </label>
-                        <input type="password" placeholder="enter password" className="input input-bordered" required />
+                        <input type="password" name='password' placeholder="enter password" className="input input-bordered" required />
                         <label className="label">
                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                         </label>
--- Easy-Repair-Client/src/Providers/AuthProvider.jsx
+++ Easy-Repair-Client/src/Providers/AuthProvider.jsx
@@ -1,5 +1,5 @@
-import { createContext, useState } from "react";
-import { getAuth } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 import app from "../Firebase/firebase.config";
 
 export const AuthContext = createContext();
@@ -9,10 +9,32 @@ const AuthProvider = ({children}) => {
    const [user, setUser] = useState(null);
    const [loading, setLoading] = useState(true);
 
+   const createUser = (email, password) =>{
+      setLoading(true);
+      return createUserWithEmailAndPassword(auth, email, password);
+   }
+
+   const singIn = (email, password) =>{
+      setLoading(true);
+      return signInWithEmailAndPassword(auth,email, password);
+   }
+
+   useEffect(()=>{
+     const unsubscribe = onAuthStateChanged(auth, currentUser =>{
+         setUser(currentUser);
+         console.log('current User', currentUser);
+         setLoading(false);
+      });
+      return () =>{
+         return unsubscribe();
+      }
+   }, [])
 
    const authInfo = {
       user,
-      loading
+      loading,
+      createUser,
+      singIn
    }
 
    return (
