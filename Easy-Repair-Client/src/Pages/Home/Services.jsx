2d4f644899d35099d436de35e9c6220232b4a58d
tajtanvvir1 <tanvirah1223@gmail.com>
2024-09-14 20:00:47 +0600

    Services Adde

---
+++ Easy-Repair-Client/src/Pages/Home/Services.jsx
@@ -0,0 +1,16 @@
+
+const Services = () => {
+   return (
+      <div>
+         <div className="mx-auto text-center">
+            <h1 className="text-3xl text-sky-600">Our Services</h1>
+            <p className="lg:w-1/2 w-3/4 mx-auto my-2">The majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
+         </div>
+         <div>
+
+         </div>
+      </div>
+   );
+};
+
+export default Services;
